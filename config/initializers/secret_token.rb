ac63040c9a59ee905855f53f5b99709d064fcb56
Andy Yong <ayong@mdsol.com>
2014-05-14 11:07:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danceschool::Application.config.secret_token = '10d7d441ffd264aa0b15e6fa8ab83ada13bde5a871c71f59eecd3c966bf6748bbc23d19a0122336a333ea3ed13147f207c036e22b5daf17b242ec7d0c94fa085'

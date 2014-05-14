90fb15682599865a6c1695b94fac9c76b9000476
Andy Yong <ayong@mdsol.com>
2014-05-14 13:56:22 -0400

    Adds presence and uniqueness validations on server side

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :username
+  
+  has_many :enrollments
+  has_many :courses, through: :enrollments, dependent: :destroy
+  
+  validates :first_name, :last_name, :email, :username, presence: { message: "cannot be blank" }
+  validates :email, :username, uniqueness: { case_sensitive: false, message: "already exists" }
 end
